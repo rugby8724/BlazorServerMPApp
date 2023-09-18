314415e35ceac9bfd977ea069b06111a88ece63a
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-09-18 16:05:46 -0500

    Add project files.

---
+++ BlazorServerMP/Models/AddressModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMP.Models
+{
+    public class AddressModel
+    {
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [StringLength(2, MinimumLength =2)]
+        public string State { get; set; }
+        [Required]
+        [StringLength (10, MinimumLength =5)]
+        public string ZipCode { get; set; }
+    }
+}
---
+++ BlazorServerMP/Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMP.Models;
+
+public class PersonModel
+{
+    [Required]
+    [StringLength(10, MinimumLength =1)]
+    public string? FirstName { get; set; }
+    [Required]
+    [StringLength(10, MinimumLength = 1)]
+    public string? LastName { get; set; }
+}
